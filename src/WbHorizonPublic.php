643228d2ecfa3b5e2b9acfae31f7c94ff6244d99
kouwa stephane <kksasteph888@gmail.com>
2022-10-07 17:03:50 +0200

    Maj

--- src/WbHorizonPublic.php
+++ src/WbHorizonPublic.php
@@ -14,27 +14,28 @@ use Drupal\Core\Url;
 class WbHorizonPublic {
 
   static public function getRouteToedit(Paragraph $Paragraph) {
-    $entity_type_id = 'paragraph';
     $link = '';
     if ($Paragraph->hasField('layout_builder__layout')) {
+      $entity_type_id = 'paragraph';
       /**
        *
        * @var \Drupal\Core\Render\Renderer $renderer
        */
       $renderer = \Drupal::service('renderer');
-      $link = [
-        '#type' => 'link',
-        '#title' => t('Config layout'),
-        '#url' => Url::fromRoute("layout_builder.overrides.$entity_type_id.view", [
-          'paragraph' => $Paragraph->id()
-        ]),
-        '#options' => [
-          'attributes' => [
-            'target' => '_blank',
-            'class' => []
+      if (!empty($Paragraph->id()))
+        $link = [
+          '#type' => 'link',
+          '#title' => t('Config layout'),
+          '#url' => Url::fromRoute("layout_builder.overrides.$entity_type_id.view", [
+            'paragraph' => $Paragraph->id()
+          ]),
+          '#options' => [
+            'attributes' => [
+              'target' => '_blank',
+              'class' => []
+            ]
           ]
-        ]
-      ];
+        ];
       // $link = $renderer->renderRoot($link);
     }
     return $link;
