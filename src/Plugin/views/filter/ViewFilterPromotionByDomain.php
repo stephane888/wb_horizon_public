505c33b1a4de0ac356cb7f5aa002b51f2a145790
kouwa stephane <kksasteph888@gmail.com>
2023-12-30 06:00:34 +0100

    Maj

---
+++ src/Plugin/views/filter/ViewFilterPromotionByDomain.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Drupal\wb_horizon_public\Plugin\views\filter;
+
+use Drupal\view_filter_promotion\Plugin\views\filter\ViewFilterPromotion;
+
+/**
+ * Permet de filtrer les entities en function du domaine encours.
+ * Elle etant la methode de definie par DomainAccessCurrentAllFilter et modifier
+ * la function de filtrer afin d'avoir un filtre complet.
+ *
+ * @ingroup views_filter_handlers
+ *
+ * @ViewsFilter("view_filter_promotion_by_domain")
+ */
+class ViewFilterPromotionByDomain extends ViewFilterPromotion {
+  
+  /**
+   *
+   * @return \Drupal\Core\Cache\ApcuBackend
+   */
+  protected function getCacheACPu() {
+    if (!$this->cacheACPu) {
+      /** @var \Drupal\domain\DomainNegotiatorInterface $domain_negotiator */
+      $domain_negotiator = \Drupal::service('domain.negotiator');
+      $current_domain = $domain_negotiator->getActiveDomain();
+      $this->cacheACPu = $this->ApcuBackendFactory->get($current_domain . $this->pluginId);
+    }
+    return $this->cacheACPu;
+  }
+  
+}
