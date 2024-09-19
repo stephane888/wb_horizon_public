ad5e0df6f1aac5d22eab508806b91a632c276776
kouwa stephane <kksasteph888@gmail.com>
2024-09-19 17:13:12 +0200

    Maj

--- src/Plugin/ManageModuleConfig/FormConfig.php
+++ src/Plugin/ManageModuleConfig/FormConfig.php
@@ -14,57 +14,48 @@ use Drupal\Core\Url;
  *   description = @Translation("Configuration for webforms of the current domain.")
  * )
  */
-class FormConfig extends ManageModuleConfigPluginBase
-{
-
+class FormConfig extends ManageModuleConfigPluginBase {
+  
   /**
    *
    * {@inheritdoc}
    * @see \Drupal\manage_module_config\ManageModuleConfigInterface::GetName()
    */
-  public function GetName()
-  {
+  public function GetName() {
     return $this->configuration['name'];
   }
-
+  
   /**
    *
    * {@inheritdoc}
    * @see \Drupal\manage_module_config\ManageModuleConfigInterface::getRoute()
    */
-  public function getRoute()
-  {
-    return Url::fromRoute(
-      'wb_horizon_public.webform_config',
-      [],
-      [
-        'query' => [
-          'destination' => \Drupal::service('path.current')->getPath()
-        ]
+  public function getRoute() {
+    return Url::fromRoute('wb_horizon_public.webform_config', [], [
+      'query' => [
+        'destination' => \Drupal::service('path.current')->getPath()
       ]
-    );
+    ]);
   }
-
+  
   /**
    *
    * {@inheritdoc}
    * @see \Drupal\manage_module_config\ManageModuleConfigInterface::getDescription()
    */
-  public function getDescription()
-  {
+  public function getDescription() {
     return $this->configuration['description'];
   }
-
+  
   /**
    *
    * {@inheritdoc}
    * @see \Drupal\manage_module_config\ManageModuleConfigPluginBase::defaultConfiguration()
    */
-  public function defaultConfiguration()
-  {
+  public function defaultConfiguration() {
     return [
-      'name' => 'Configuration des Formulaires',
-      'description' => "Configuration for webforms of the current domain.",
+      'name' => t('Forms Configuration'),
+      'description' => t("Configuration for webforms of the current domain."),
       'enable' => true,
       'icon_svg_class' => 'text-white btn-lg bg-dark',
       'icon_svg' => '<svg fill="currentColor" width="1em" height="1em" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path d="M22 6H2a1 1 0 0 0-1 1v3a1 1 0 0 0 1 1h20a1 1 0 0 0 1-1V7a1 1 0 0 0-1-1m0 4H2V7h20zh.001M22 17H2a1 1 0 0 0-1 1v3a1 1 0 0 0 1 1h20a1 1 0 0 0 1-1v-3a1 1 0 0 0-1-1m0 4H2v-3h20zh.001M10 14v1H2v-1zM2 3h8v1H2z"/><path fill="none" d="M0 0h24v24H0z"/></svg>'
