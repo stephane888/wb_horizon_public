dabfc63d691673a6fe8015002e7d00cd8677fa66
kouwa stephane <kksasteph888@gmail.com>
2024-12-19 09:33:56 +0100

    Maj

--- src/Services/CreateUpdatePage.php
+++ src/Services/CreateUpdatePage.php
@@ -9,6 +9,7 @@ use Drupal\creation_site_virtuel\Entity\SiteInternetEntity;
 use Drupal\creation_site_virtuel\Entity\SiteTypeDatas;
 use Drupal\menu_link_content\Entity\MenuLinkContent;
 use Drupal\apivuejs\Services\DuplicateEntityReference;
+use Drupal\lesroidelareno\lesroidelareno;
 
 /**
  *
@@ -28,9 +29,36 @@ final class CreateUpdatePage {
    * @return \Drupal\creation_site_virtuel\Entity\SiteInternetEntity
    */
   public function createUpdatePage(array $values, array $configs) {
-    dd($configs);
+    $uid = \Drupal::currentUser()->id();
+    $mene_item = [];
+    if ($configs['action_to_do'] == 'add_menu') {
+      if ($configs['content_to_display'] == 'page') {
+        if ($sections = $this->buildParagraphFromModel($configs['load_content'])) {
+          $values['layout_paragraphs'] = $sections;
+        }
+      }
+    }
+    $mene_item['title'] = $configs['menu_name'];
+    $values['name'] = $configs['menu_name'];
+    //
+    $values['type'] = 'rc_web';
+    
     $entity = SiteInternetEntity::create($values);
-    // $entity->save();
+    $entity->save();
+    // Create or update menu.
+    $menu_id = lesroidelareno::getCurrentPrefixDomain(false) . '-main';
+    $mene_item['menu_name'] = $menu_id;
+    $mene_item['bundle'] = $menu_id;
+    $mene_item['wbh_user_id'] = $uid;
+    $mene_item['link'] = [
+      'uri' => 'internal:/site-internet-entity/' . $entity->id(),
+      'title' => null,
+      'options' => []
+    ];
+    $mene_item['external'] = 0;
+    $mene_item['expanded'] = 1;
+    $mene_item['weight'] = 10;
+    $this->createUpdateMenu($mene_item);
     return $entity;
   }
   
@@ -39,7 +67,7 @@ final class CreateUpdatePage {
    * @param array $values
    * @return \Drupal\menu_link_content\Entity\MenuLinkContent
    */
-  public function createUpdateMenu(array $values) {
+  protected function createUpdateMenu(array $values) {
     $entity = MenuLinkContent::create($values);
     $entity->save();
     return $entity;
@@ -50,11 +78,11 @@ final class CreateUpdatePage {
    * @param string $model
    * @param array $values
    */
-  public function buildParagraphFromModel(string $model, array &$values) {
+  protected function buildParagraphFromModel(string $model) {
     $sections = [];
     switch ($model) {
       case 'page_text':
-        $id = '473';
+        $id = 473;
         $sections = $this->getSectionsFromModel($id);
         break;
       
@@ -73,9 +101,15 @@ final class CreateUpdatePage {
       // Pour reduire les données qui envoye vers la duplications.
       // On initialise une nouvelle entité avec uniquement des sections.
       $values = [
+        'site_internet_entity_type' => 'rc_web',
         'layout_paragraphs' => $SiteTypeDatas->get('layout_paragraphs')->getValue()
       ];
-      if ($entity = $this->DuplicateEntityReference->duplicateEntity(SiteTypeDatas::create($values), false)) {
+      
+      $setValues = [
+        \Drupal\domain_access\DomainAccessManagerInterface::DOMAIN_ACCESS_FIELD => \Drupal\lesroidelareno\lesroidelareno::getCurrentDomainId(),
+        \Drupal\domain_source\DomainSourceElementManagerInterface::DOMAIN_SOURCE_FIELD => \Drupal\lesroidelareno\lesroidelareno::getCurrentDomainId()
+      ];
+      if ($entity = $this->DuplicateEntityReference->duplicateEntity(SiteTypeDatas::create($values), false, [], $setValues, true)) {
         return $entity->get('layout_paragraphs')->getValue();
       }
     }
